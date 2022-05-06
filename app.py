d8dd1b07a92118563f628ba0215e6dfcb483cbba
Dmitry Yokubauskas <yokubauskas@mail.ru>
2022-05-06 21:41:01 +0300

    update app

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_sock import Sock
 import time
 import numpy as np
 
 app = Flask(__name__)
 sock = Sock(app)
-control_value = 0
+data = {}
 
 W_res = np.load("model2.npy")
 W_out, W, W_in, W_c = W_res[:, :4].T, W_res[:, 4:304], W_res[:, 304:308], W_res[:, 308:]
@@ -25,35 +25,50 @@ def lorenz_step(y, rho = 28, dt = 0.002):
 
     return r
 
-def esn_step(y, s, c):
-    # print(W_in.shape, W.shape, W_c.shape, y.shape, s.shape, c.shape)
-    s = np.tanh(W_in @ y + W @ s + W_c @ c)
-    y = W_out @ s
-    return y, s
+def esn_step(_d):
+    _d["states"] = np.tanh(W_in @ _d["outputs"] + 
+        W @ _d["states"] + W_c @ _d["controls"])
+    _d["outputs"] = W_out @ _d["states"]
+    _d["outputs"][0] = 1
+
+def update_data(addr):
+    # print(data.keys())
+    if(addr in data): data[addr]["timer"] = time.time()
+    for a in list(data.keys()):
+        if(time.time() - data[a]["timer"] > 0.5):
+            del data[a] 
+    if(addr in data): return
+
+    data[addr] = {}
+    data[addr]["controls"] = np.array([0.2, -0.2])
+    data[addr]["states"] = np.zeros((300,))
+    data[addr]["outputs"] = np.array([1, -0.38557625, -0.47640575,  0.0317102 ])
+    data[addr]["timer"] = time.time()
+
 
 @sock.route('/to_server')
 def to_server(sock):
     global control_value
     while True:
-        data = sock.receive()
-        control_value = np.array(list(map(float, data.split())))
-        # print(control_value)
+        update_data(request.remote_addr)
+
+        recieved = sock.receive()
+        _d = data[request.remote_addr]
+        _d["controls"] = np.array(list(map(float, recieved.split())))
 
 @sock.route('/from_server')
 def from_server(sock):
     global control_value
 
-    control_value = np.array([0.2, -0.2])
-    y = np.array([1, -0.38557625, -0.47640575,  0.0317102 ])
-    s = np.zeros((300,))
     while True:
-        time.sleep(1 / 30.)
-        y, s = esn_step(y, s, control_value)
-        y[0] = 1
-        sock.send(" ".join(map(str, y[1:])))
+        update_data(request.remote_addr)
 
+        time.sleep(1 / 30.)
+        _d = data[request.remote_addr]
+        esn_step(_d)
+        sock.send(" ".join(map(str, _d["outputs"][1:])))
 
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", debug=True)
